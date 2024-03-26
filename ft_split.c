45f8eda1a270c551d820c2032e0fd5cb6f42b4a7
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2024-03-26 16:17:09 +0100

    split

---
+++ ft_split.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igngonza <igngonza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 15:54:56 by igngonza          #+#    #+#             */
+/*   Updated: 2024/03/26 16:15:42 by igngonza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+static size_t	ft_toklen(const char *s, char c)
+{
+	size_t	ret;
+
+	ret = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			++ret;
+			while (*s && *s != c)
+				++s;
+		}
+		else
+			++s;
+	}
+	return (ret);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char **ret;
+	size_t i;
+	size_t len;
+
+	if (!s)
+		return (0);
+	i = 0;
+	ret = malloc(sizeof(char *) * (ft_toklen(s, c) + 1));
+	if (!ret)
+		return (0);
+	while (*s)
+	{
+		if (*s != c)
+		{
+			len = 0;
+			while (*s && *s != c && ++len)
+				++s;
+			ret[i++] = ft_substr(s - len, 0, len);
+		}
+		else
+			++s;
+	}
+	ret[i] = 0;
+	return (ret);
+}
