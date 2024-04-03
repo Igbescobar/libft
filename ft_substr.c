cecafbf8e2a881790120882a7908684be3e11839
Ignacio <igbescobar@gmail.com>
2024-04-03 20:25:24 +0200

    substr done

--- ft_substr.c
+++ ft_substr.c
@@ -3,23 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igngonza <igngonza@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: igngonza <igngonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 16:37:53 by igngonza          #+#    #+#             */
-/*   Updated: 2024/04/01 19:11:40 by igngonza         ###   ########.fr       */
+/*   Updated: 2024/04/03 20:20:48 by igngonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*result;
 
-	if (!s)
-		return (0);
-	if (ft_strlen(s) < start)
-		len = 0;
+	if (!s || start > ft_strlen(s))
+		return (ft_strdup(""));
 	if (ft_strlen(s + start) < len)
 		len = ft_strlen(s + start);
 	result = malloc((len + 1) * sizeof(char));
@@ -33,9 +32,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (result);
 }
 
-/*int	main(void)
+int	main(void)
 {
-	char *arr = "hello there";
-	printf("%s\n", ft_substr(arr, 6, 2));
+	char	*arr;
+
+	arr = "hello there";
+	printf("%s\n", ft_substr("", 1, 1));
 	return (0);
-}*/
+}
