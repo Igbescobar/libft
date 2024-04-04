2bb86e16a47bf6e79cf1b8080b7761db72de81cb
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2024-04-04 17:29:20 +0200

    fixed strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: igngonza <igngonza@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 15:18:58 by igngonza          #+#    #+#             */
-/*   Updated: 2024/04/03 21:29:25 by igngonza         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:28:41 by igngonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,26 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != c && *s != '\0')
-		s++;
-	if (*s == '\0' && c != '\0')
+	int				i;
+	unsigned char	ch;
+
+	if (!s)
 		return (0);
-	return ((char *)s);
+	i = 0;
+	ch = c + '\0';
+	while (s[i] != '\0')
+	{
+		if (s[i] == ch)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (ch == '\0')
+		return ((char *)(s + i));
+	return (0);
 }
 
 /*int	main(void)
 {
-	printf("%s\n", ft_strchr("teste", 1024));
-	printf("%s\n", strchr("teste", 1024));
+	printf("%s\n", ft_strchr(((void *)0), '\0'));
+	printf("%s\n", strchr(((void *)0), '\0'));
 }*/
