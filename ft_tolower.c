20ea1cf55ffcf88627f5c020cd4c7e4ea9da13b6
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2024-04-15 16:57:57 +0200

    Added main to tolower

--- ft_tolower.c
+++ ft_tolower.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: igngonza <igngonza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: igngonza <igngonza@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 12:43:47 by igngonza          #+#    #+#             */
-/*   Updated: 2024/03/19 16:36:48 by igngonza         ###   ########.fr       */
+/*   Updated: 2024/04/15 16:57:32 by igngonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_tolower(int c)
 {
@@ -18,3 +20,8 @@ int	ft_tolower(int c)
 	}
 	return (c);
 }
+/*int	main(void)
+{
+	printf("%d\n", ft_tolower('h'));
+	return (0);
+}*/
