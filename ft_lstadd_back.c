bdf4f1d81a029dc61216452c75f837b8f9a10c8b
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2024-12-16 16:23:41 +0100

    lst_add_back mejor

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: igngonza <igngonza@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 10:25:19 by igngonza          #+#    #+#             */
-/*   Updated: 2024/11/18 18:32:19 by igngonza         ###   ########.fr       */
+/*   Updated: 2024/12/16 16:19:32 by igngonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,16 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	if (!lst || !new)
-		return ;
-	while (*lst)
-		lst = &(*lst)->next;
-	*lst = new;
+	t_list	*tmp;
+
+	if (lst)
+	{
+		if (*lst == NULL)
+			*lst = new;
+		else
+		{
+			tmp = ft_lstlast(*(lst));
+			tmp->next = new;
+		}
+	}
 }
