e72d89854c9cddcab6490e6207c4b9ed5a9b65ac
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2024-12-17 15:01:47 +0100

    ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: igngonza <igngonza@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 11:20:06 by igngonza          #+#    #+#             */
-/*   Updated: 2024/11/18 18:31:48 by igngonza         ###   ########.fr       */
+/*   Updated: 2024/12/17 14:59:24 by igngonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,29 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
-	t_list	*new_elem;
-	t_list	*next;
+	t_list	*new;
+	t_list	*tmp;
+	t_list	*begin;
 
-	if (!lst || !f)
-		return (NULL);
-	new_list = NULL;
-	while (lst)
+	if (lst)
 	{
-		next = lst->next;
-		new_elem = ft_lstnew(f(lst->content));
-		if (!new_elem)
-		{
-			ft_lstclear(&new_list, del);
+		tmp = lst;
+		begin = ft_lstnew(f(tmp->content));
+		if (!begin)
 			return (NULL);
+		tmp = tmp->next;
+		while (tmp)
+		{
+			new = ft_lstnew(f(tmp->content));
+			if (!new)
+			{
+				ft_lstclear(&begin, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&begin, new);
+			tmp = tmp->next;
 		}
-		ft_lstadd_back(&new_list, new_elem);
-		lst = next;
+		return (begin);
 	}
-	return (new_list);
+	return (NULL);
 }
